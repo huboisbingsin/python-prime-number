36a414db2191d58a552940a50bec38755da360ac
huboisbingsin <1004parkcheamin@gmail.com>
2026-09-24 11:57:40 +0900

    "feat: 2 부터 100 까지 소수 판별 기본 로직 구현

--- prime-number.py
+++ prime-number.py
@@ -1,4 +1,10 @@
-# prime-number.py -소수구하기프로그램기본틀
+# prime-number.py -소수구하기기본로직완성
 print("1부터100 사이의소수를구합니다.")
-for num in range(1, 101):
-    pass
+for num in range(2, 101):
+    is_prime= True
+    for i in range(2, num):
+        if num % i == 0:
+            is_prime = False
+            break
+    if is_prime:
+        print(num, end=" ")
